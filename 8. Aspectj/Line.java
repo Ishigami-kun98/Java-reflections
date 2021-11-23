29fa0500bd24b54f3200f9e6a913dc27eba51c8b
Ishigami-kun98 <48060336+Ishigami-kun98@users.noreply.github.com>
2021-11-23 13:08:42 +0100

    pausa

--- 8. Aspectj/Line.java
+++ 8. Aspectj/Line.java
@@ -1,3 +1,5 @@
+import jdk.internal.org.jline.utils.Display;
+
 public class Line implements FigureElement{
     private Point p1, p2;
     public Point getP1(){ return p1; }
@@ -6,6 +8,7 @@ public class Line implements FigureElement{
         this.p1 = p1;
         Display.update();
     }
+    
     public void setP2(Point p2){
         this.p2 = p2;
         Display.update();
