05ce3a93a40868102ad8871623c0810183bd6453
Ishigami-kun98 <linhu2813004615@gmail.com>
2021-11-30 14:39:20 +0100

    DumpMethodAdvance
    adding discover to normal class and it's super class

--- 1. DumpMethod/DumpMethod.java
+++ 1. DumpMethod/DumpMethod.java
@@ -2,14 +2,29 @@
 import java.lang.reflect.Method;
 
 public class DumpMethod {
+
     public static void main(String[] args) {
 
-        // this class find all interfaces that the target class have
+        // this class find all interfaces and superclass that the target class have
 
         try {
-            Class<?>[] ilist = getInterface(Class.forName("TestClass"));
+            Class<?> cls = Class.forName("TestClass");
+            Class<?>[] ilist = getInterface(cls);
+            Class<?>[] scls = cls.getClasses();
+
+            for (Class<?> sclsp : scls) {
+                if (!sclsp.isInterface()) {
+                    // we are in class
+                    System.out.println("Class " + sclsp.getName());
+                    if (isSuperClass(sclsp)) {
+                        // is a super class
+                        System.out.println("Is a super class")
+                    }
+                }
+            }
             // This is using args
             // Class<?>[] ilist = getInterface(Class.forName(args[0]));
+
             for (Class<?> iface : ilist) {
                 Method[] methods = iface.getMethods();
                 for (Method method : methods) {
@@ -26,4 +41,24 @@ public class DumpMethod {
         System.out.println("the interface he got is : " + cls.getInterfaces().length);
         return cls.getInterfaces();
     }
+
+    static boolean isSuperClass(Class<?> cls) {
+        if (cls.getSuperclass().equals(Object.class)) {
+            return false;
+        }
+        return true;
+    }
+
+    /*
+     * static Class<?> getSuperClasses(Class<?> cls) {
+     * if (cls.equals(Object.class)) {
+     * System.out.println("finish");
+     * return cls;
+     * }
+     * System.out.println(
+     * "The className is " + cls.getName() + " \n it's super class is " +
+     * cls.getSuperclass().getName());
+     * return getSuperClasses(cls.getSuperclass());
+     * }
+     */
 }
