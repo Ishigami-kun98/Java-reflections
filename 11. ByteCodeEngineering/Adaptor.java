2ff8a5c4673e1109446738a0b370969619c2a469
Ishigami-kun98 <48060336+Ishigami-kun98@users.noreply.github.com>
2021-12-13 10:40:39 +0100

    something

--- 11. ByteCodeEngineering/Adaptor.java
+++ 11. ByteCodeEngineering/Adaptor.java
@@ -7,27 +7,58 @@ public class Adaptor implements Translator{
     CtMethod insert;
     CtClass cls;
     @Override
+    public void start(ClassPool pool) throws NotFoundException, CannotCompileException {
+        try {
+            cls = pool.get("java.lang.StringBuilder");
+            CtMethod[] methods = cls.getDeclaredMethods("append");
+            for (CtMethod ctMethod : methods) {
+                
+                if(ctMethod.getSignature().equals("([C)Ljava/lang/StringBuilder;")){
+                    append = ctMethod;
+                }
+            }
+            CtMethod[] insertMethods = cls.getDeclaredMethods("insert");
+            for (CtMethod ctMethod : insertMethods) {
+                if(ctMethod.getSignature().equals("(ILjava/lang/String;)Ljava/lang/StringBuilder;")){
+                    insert = ctMethod;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    @Override
     public void onLoad(ClassPool pool, String arg1) throws NotFoundException, CannotCompileException {
         try {
             CtClass clazz = pool.get("MyApp");
             CtMethod ctMethod = clazz.getDeclaredMethod("main");
-            ctMethod.addLocalVariable("AppendResutl", CtClass.longType);
+            ctMethod.addLocalVariable("AppendResult", CtClass.longType);
             ctMethod.addLocalVariable("InsertResult", CtClass.longType);
-            ctMethod.insertBefore("AppendResult = 0");
-            ctMethod.insertBefore("InsertResult = 0");
+            ctMethod.insertBefore("AppendResult = 0;");
+            ctMethod.insertBefore("InsertResult = 0;");
+            ctMethod.insertAfter("if(AppendResult < InsertResult){" +
+                "System.out.println(\"Append takes less time than Insert\");"+
+                "}else {"+
+                "System.out.println(\"Insert takes less time than Append\");}"
+            );
             ctMethod.instrument(new ExprEditor(){
                 public void edit(final MethodCall m) throws CannotCompileException{
-                    if(m.getClassName().equals("java.lang.StringBuilder") && m.getMethodName().equals(append.getName()) && m.getSignature().equals(append.getSignature())){
+                    if(m.getClassName().equals("java.lang.StringBuilder") && 
+                    m.getMethodName().equals(append.getName()) && 
+                    m.getSignature().equals(append.getSignature())){
                         m.replace("{long appendStartMs = System.nanoTime(); " + 
                         "$_ = $proceed($$);" +
                         "long appendEndMs = System.nanoTime();" +
+                        "AppendResult = appendEndMs - appendStartMs;" +
                         "System.out.println(\"Append ExecutionTime: \" + AppendResult);}"
                         );
                     }
-                    if(m.getClassName().equals("java.lang.StringBuilder") && m.getMethodName().equals(insert.getName()) && m.getSignature().equals(insert.getSignature())){
+                    else if (m.getClassName().equals("java.lang.StringBuilder") && m.getMethodName().equals(insert.getName()) && m.getSignature().equals(insert.getSignature())){
                         m.replace("{long appendStartMs = System.nanoTime(); " + 
                         "$_ = $proceed($$);" +
-                        "long appendEndMs = System.nanoTime();" +
+                        "long appendEndMs = System.nanoTime(); " +
+                        "AppendResult = appendEndMs - appendStartMs;" +
                         "System.out.println(\"insert ExecutionTime: \" + AppendResult);}"
                         );
                     }
@@ -37,29 +68,7 @@ public class Adaptor implements Translator{
             e.printStackTrace();//TODO: handle exception
         }
     }
-    @Override
-    public void start(ClassPool pool) throws NotFoundException, CannotCompileException {
-        try {
-            cls = pool.get("java.lang.StringBUilder");
-            CtMethod[] methods = cls.getDeclaredMethods("append");
-            for (CtMethod ctMethod : methods) {
-                System.out.println(ctMethod.getSignature());
-                if(ctMethod.getSignature().equals("[C)Ljava/lang/StringBuilder;")){
-                    append = ctMethod;
-                }
-            }
-            CtMethod[] insertMethods = cls.getDeclaredMethods("insert");
-            for (CtMethod ctMethod : insertMethods) {
-                System.out.println(ctMethod.getSignature());
-                if(ctMethod.getSignature().equals("(ILjava/lang/String;)Ljava/lang/-StringBuilder;")){
-                    insert = ctMethod;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-    }
+   
 
     
 }
--- 11. ByteCodeEngineering/BCEL.java
+++ 11. ByteCodeEngineering/BCEL.java
@@ -1,41 +1,4 @@
-import org.apache.bcel.Repository;
-import org.apache.bcel.classfile.*;
-import org.apache.bcel.generic.*;
-import java.lang.annotation.*;
-import java.util.Arrays;
 
-import javassist.bytecode.AccessFlag;
 public class BCEL {
-
-    public static void printCode(Method[] methods){
-        for(var m : methods){
-            System.out.println(m);
-            Code body = m.getCode();
-            if(body != null){
-                System.out.println(body);
-            }
-        }
-    }
-
-        public static void main(String[] args){
-            try{
-                JavaClass clazz = Repository.lookupClass("java.lang.String");
-                printCode(clazz.getMethods());
-                //Create a class
-                ClassGen cg = new ClassGen("Test", "java.lang.Object", "Test.java", AccessFlag.PUBLIC,null);
-                ConstantPoolGen cp = cg.getConstantPool();
-                FieldGen gf = new FieldGen(AccessFlag.PUBLIC, org.apache.bcel.generic.Type.INT, "field1", cp);
-                InstructionList il = new InstructionList();
-                cg.addField(gf.getField());
-                MethodGen mg = new MethodGen(AccessFlag.PUBLIC, org.apache.bcel.generic.Type.VOID, org.apache.bcel.generic.Type.NO_ARGS, null, "MethodACaso", "Test", il, cp);
-                mg.setMaxStack();
-                cg.addMethod(mg.getMethod());
-                System.out.println(cg.getClassName() + " " +  Arrays.toString(cg.getFields()) + cg.getMethodAt(0).getName());                
-            }catch(ClassNotFoundException e){
-                e.printStackTrace();
-                
-            }
-        }
-    
     
 }
